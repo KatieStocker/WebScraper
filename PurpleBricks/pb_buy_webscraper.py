a2e5014effff4794b1cd334b159ec55bf0bf23c4
KatieStocker <stocker_katie@aol.co.uk>
2023-04-11 17:44:52 +0100

    updated logic to loop through each all pages present

--- PurpleBricks/pb_buy_webscraper.py
+++ PurpleBricks/pb_buy_webscraper.py
@@ -14,32 +14,25 @@ def main():
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        lastPage = getLastPageNumber(soup)
+        # find the elements on the page that you want to extract data from
+        # e.g. the price of the properties
+        prices = soup.find_all('strong', {'class': 'property-cardstyled__StyledPrice-sc-15g6092-7 cWDEnM'})
+        addresses = soup.find_all('span', {'class': 'property-cardstyled__StyledAddress-sc-15g6092-10 kLxiCm'})
+        features = soup.find_all('span', {'class': 'property-cardstyled__StyledTitle-sc-15g6092-9 hpHzPf'})
+        web_links = soup.find_all('a', {'class': 'property-cardstyled__StyledLink-sc-15g6092-1 eQIvCR'})
 
+        initialiseDataObject(data, len(prices))
+
+        populateDataObject(prices, "price", data, pages)
+        populateDataObject(addresses, "address", data, pages)
+        populateDataObject(features, "features", data, pages)
+        populateDataObject(web_links, "web_link", data, pages)
+
+        lastPage = getLastPageNumber(soup)
 
         if lastPage == index:
             break
         index += 1
-
-    # send a GET request to the URL
-    response = requests.get(PB_BUY_URL)
-
-    # parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # find the elements on the page that you want to extract data from
-    # e.g. the price of the properties
-    prices = soup.find_all('strong', {'class': 'property-cardstyled__StyledPrice-sc-15g6092-7 cWDEnM'})
-    addresses = soup.find_all('span', {'class': 'property-cardstyled__StyledAddress-sc-15g6092-10 kLxiCm'})
-    features = soup.find_all('span', {'class': 'property-cardstyled__StyledTitle-sc-15g6092-9 hpHzPf'})
-    web_links = soup.find_all('a', {'class': 'property-cardstyled__StyledLink-sc-15g6092-1 eQIvCR'})
-
-    initialiseDataObject(data, len(prices), index)
-
-    populateDataObject(prices, "price", data, index)
-    populateDataObject(addresses, "address", data, index)
-    populateDataObject(features, "features", data, index)
-    populateDataObject(web_links, "web_link", data, index)
 
     createOutputs(data, ['price', 'address'], 'buy', 'purplebricks')
 
@@ -50,14 +43,14 @@ def getLastPageNumber(soup):
     lastPage = paginationList[len(paginationList) - 2]
     return int(lastPage.text)
 
-def initialiseDataObject(data, length, index):
+def initialiseDataObject(data, length):
     count = 0
     while count < length:
         data.append({"price": "", "address": "", "features": "", "web_link": ""})
         count+=1
 
 def populateDataObject(items, itemType, dataObject, index):
-    count = 0
+    count = 0 + (index * 10)
     for item in items:
         stringToAppend = ""
         if itemType == "web_link":
